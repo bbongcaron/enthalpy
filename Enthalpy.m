33d31bdf786b0d372b2548dea83d5eee973eb6bd
Brenton Bongcaron <brenton3926@gmail.com>
2020-11-20 21:56:41 -0500

    Modified GUI to adjust when water is the component

--- Enthalpy.m
+++ Enthalpy.m
@@ -1,5 +1,5 @@
 function [] = Enthalpy()
-    [component, refTemp, state, temp] = userInput();
+    [component, refTemp, state, temp, pressure] = userInput();
     if strcmpi(component, 'water') && refTemp == 0.01
         % case to be worked on later
     else
@@ -7,7 +7,7 @@ function [] = Enthalpy()
     end
 end
 
-function [component, refTemp, state, temp] = userInput()   
+function [component, refTemp, state, temp, pressure] = userInput()   
     % may be inefficient run-time wise to grab component column twice, 
     % however big O is still O(n), where
     % n is number of components in heatCapacity.xlsx
@@ -25,18 +25,29 @@ function [component, refTemp, state, temp] = userInput()
  %% Listdlg to choose component
     componentIndex = listdlg('ListString', matComp, ...
         'PromptString', 'Select a component:', ...
-        'ListSize', [400,400], 'SelectionMode', 'single')
+        'ListSize', [400,400], 'SelectionMode', 'single');
     component = matComp{componentIndex};
     refTemp = 0.01;
-    temp = 0.01;
-    promptCell = {'Reference Temp [°C]', ...
+    temp = NaN;
+    pressure = NaN;
+    if strcmpi(component, 'water')
+        promptCell = {['For steam tables, Reference Temp = 0.01°C.  ', ...
+            '   Leave unknown/unecessary quantities blank. ', ...
+            'Temperature[°C]'], 'Pressure [kPa]'...
+             "Type 'liquid' or 'gas' (without quotes)"};
+        cell = inputdlg(promptCell, 'State Variables', [1 50]);
+        temp = str2double(cell{1});
+        pressure = str2double(cell{2});
+        state = cell{3};
+    else
+        promptCell = {'Reference Temp [°C]', ...
                     'Temperature[°C]',...
                     "Type 'solid', 'liquid' or 'gas' (without quotes)"};
-    tempCell = inputdlg(promptCell, 'Temperature', [1 40]);
-    refTemp = str2double(tempCell{1});
-    temp = str2double(tempCell{2});
-    state = tempCell{3};
-    
+        tempCell = inputdlg(promptCell, 'Temperature', [1 40]);
+        refTemp = str2double(tempCell{1});
+        temp = str2double(tempCell{2});
+        state = tempCell{3};
+    end
 end
 
 function [enthalpy] = heatCapacity(component, refTemp, temp)
