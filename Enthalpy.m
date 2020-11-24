f10526f95a27f90bae9e84b2eab343156fccc5fb
Brenton Bongcaron <brenton3926@gmail.com>
2020-11-23 20:57:07 -0500

    Added enthalpy calc thru heat capacity + temp check

--- Enthalpy.m
+++ Enthalpy.m
@@ -1,10 +1,11 @@
-function [] = Enthalpy()
+function [] = enthalpy()
     [component, refTemp, state, temp, pressure] = userInput();
     if strcmpi(component, 'water') && refTemp == 0.01
         % case to be worked on later
     else
-        enthalpy = heatCapacity(component, refTemp, temp)
+        enthalpy = heatCapacity(component, refTemp, temp, state)
     end
+    
 end
 
 function [component, refTemp, state, temp, pressure] = userInput()   
@@ -50,12 +51,46 @@ function [component, refTemp, state, temp, pressure] = userInput()
     end
 end
 
-function [enthalpy] = heatCapacity(component, refTemp, temp)
+function [enthalpy] = heatCapacity(component, refTemp, temp, state)
     enthalpy = 0;
+    A = 0; B = 0; C = 0; D = 0;
     if refTemp == temp
         return
     end
-    [matA, matB, matC, matD] = readvars('heatCapacity.xlsx', 'Range', 'C2:F62');
-    
+%% Read in heatCapacity.xlsx data
+    [matComp, matState, matA, matB, matC, matD, tUnits, matLowT, matHighT] = ...
+        readvars('heatCapacity.xlsx', 'Range', 'A2:I62');
+%% Linear search for the correct component O(n)
+    for i = 1:length(matA)
+        if strcmpi(component, matComp(i)) && strcmpi(state, matState(i))
+            A = matA(i);
+            if (~isnan(matB(i))); B = matB(i); end
+            if (~isnan(matC(i))); C = matC(i); end
+            if (~isnan(matD(i))); D = matD(i); end
+            % Kelvin conversion if necessary
+            if strcmpi(tUnits(i), 'K')
+                temp = temp + 273.15;
+                refTemp = refTemp + 273.15;
+            end
+            % Check if refTemp and temp are in acceptable temperature range
+            if refTemp < matLowT(i) || temp < matLowT(i) ...
+                    || refTemp > matHighT(i) || temp > matHighT(i)
+                enthalpy = NaN;
+                line1 = "One or more temperatures are outside valid " + ...
+                    "temperature range for " + component + " " + ...
+                    state + ":";
+                line2 = "Reference Temperature : " + refTemp + tUnits(i);
+                line3 = "Temperature : " + temp + tUnits(i);
+                line4 = "Temperature Range : " + matLowT(i) + tUnits(i) ...
+                    + " <= T <= " + matHighT(i) + tUnits(i);
+                msgbox({line1;line2;line3;line4}, "Outside Valid Temperature Range!");
+            end
+            break
+        end
+    end
+ %% Create heat capacity equation, solve for enthalpy
+    syms T
+    heatCap = A*T + B*T^2 + C*T^3 + D*T^4;
+    enthalpy = double(int(heatCap, T, refTemp, temp));
 end
 
